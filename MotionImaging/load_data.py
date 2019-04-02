ff2d2d0bad000c08e27371e8a955661351d15dd9
zcc <listenzcc@mail.bnu.edu.cn>
2019-04-02 13:47:53 +0800

    modify load_data, show raw infomations

--- MotionImaging/load_data.py
+++ MotionImaging/load_data.py
@@ -4,22 +4,15 @@ import matplotlib.pyplot as plt
 import mne
 import numpy as np
 import os
-from sklearn.decomposition import PCA
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import ShuffleSplit
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.svm import SVC
 
 
 file_dir = os.path.join('D:\\', 'RSVP_MEG_experiment', 'rawdata',
-                        '20190326', '20190326',
+                        '20190326_MI_MEG_%s',
                         'S%02d_lixiangTHU_20190326_%02d.ds')
 
 event_id = dict(MI1=1, MI2=2)
 tmin, t0, tmax = -1, 0, 4
-freq_l, freq_h = 1, 270
+freq_l, freq_h = 1, 120
 decim = 1
 
 # Make defaults
@@ -28,7 +21,7 @@ reject = dict(mag=5e-12)
 
 # Prepare rawobject
 raw_files = [mne.io.read_raw_ctf(
-    file_dir % (2, j), preload=True) for j in [1, 2]]
+    file_dir % ('maxuelin', 2, j), preload=True) for j in [1, 2]]
 raw = mne.concatenate_raws(raw_files)
 raw.filter(freq_l, freq_h, fir_design='firwin')
 picks = mne.pick_types(raw.info, meg=True, ref_meg=False, exclude='bads')
@@ -43,74 +36,31 @@ epochs = mne.Epochs(raw, event_id=event_id, events=events,
                     reject=reject, preload=True)
 epochs.resample(120, npad="auto")
 
+ch_names = [e[0:5] for e in epochs.ch_names]
+layout_all = mne.find_layout(epochs.info)
+ex = [e for e in layout_all.names if e not in ch_names]
+# layout_all.plot(picks=[layout_all.names.index(e) for e in ex])
+layout = mne.find_layout(epochs.info, exclude=ex)
+
 # Get evoked
-if False:
-    evoked_1 = epochs['MI1'].average()
-    evoked_2 = epochs['MI2'].average()
-    fig, axes = plt.subplots(2, 1)
-    a0 = axes[0]
-    f1 = evoked_1.plot_joint(show=False)
-    f2 = evoked_2.plot_joint(show=False)
-    plt.show()
+evoked_1 = epochs['MI1'].average()
+evoked_2 = epochs['MI2'].average()
 
+# Plot evoked
+fig, axes = plt.subplots(2, 1)
+evoked_1.plot(axes=axes[0], spatial_colors=True, show=False)
+evoked_2.plot(axes=axes[1], spatial_colors=True, show=False)
+f1 = evoked_1.plot_joint(show=False)
+f2 = evoked_2.plot_joint(show=False)
 
-# MVPA
-epochs_train = epochs.copy().crop(tmin=1.0, tmax=2.0)
-labels = epochs.events[:, -1]
-epochs_data = epochs.get_data()
-epochs_data_train = epochs_train.get_data()
-
-
-# CSP LR demo
-csp = mne.decoding.CSP(n_components=6, reg=None,
-                       log=True, norm_trace=False)
-# csp.fit_transform(epochs_data_train, labels)
-# csp.plot_patterns(epochs.info, show=False)
-cv = ShuffleSplit(10, test_size=0.2)
-lr = LogisticRegression(solver='lbfgs')
-svm = SVC(gamma='auto')
-lda = LinearDiscriminantAnalysis()
-clf = make_pipeline(mne.decoding.Scaler(epochs_train.info),
-                    csp,
-                    mne.decoding.Vectorizer(),
-                    lr)
-scores = mne.decoding.cross_val_multiscore(
-    clf, epochs_data_train, labels, cv=cv, n_jobs=12)
-class_balance = np.mean(labels == labels[0])
-class_balance = max(class_balance, 1. - class_balance)
-print("Classification accuracy: %f / Chance level: %f" % (
-    np.mean(scores), class_balance))
-
-
-# MVPA time_resolution
-sfreq = epochs.info['sfreq']
-w_length = int(sfreq * 0.5)   # running classifier: window length
-w_step = int(sfreq * 0.1)  # running classifier: window step size
-w_start = np.arange(0, epochs_data.shape[2] - w_length, w_step)
-scores_rep = []
-for _ in range(10):
-    print(_)
-    score_this_rep = []
-    for train_idx, test_idx in cv.split(epochs_data):
-        y_train, y_test = labels[train_idx], labels[test_idx]
-        X_train = epochs_data_train[train_idx]
-        clf.fit(X_train, y_train)
-        score_this_window = []
-        for n in w_start:
-            X_test = epochs_data[test_idx][:, :, n:(n+w_length)]
-            score = clf.score(X_test, y_test)
-            score_this_window.append(score)
-        score_this_rep.append(score_this_window)
-    scores_rep.append(score_this_rep)
-scores_windows = np.mean(np.array(scores_rep), 0)
-# Plot scores over time
-w_times = (w_start + w_length / 2.) / sfreq + epochs.tmin
-plt.figure()
-plt.plot(w_times, np.mean(scores_windows, 0), label='Score')
-plt.axvline(0, linestyle='--', color='k', label='Onset')
-plt.axhline(0.5, linestyle='-', color='k', label='Chance')
-plt.xlabel('time (s)')
-plt.ylabel('classification accuracy')
-plt.title('Classification score over time')
-plt.legend(loc='lower right')
+# Temporal-frequency
+n_cycles = 2
+freqs = np.linspace(freq_l, freq_h, 10)
+freqs = np.logspace(*np.log10([freq_l, freq_h]), num=20)
+power, itc = mne.time_frequency.tfr_morlet(
+    epochs, freqs=freqs, n_cycles=n_cycles,
+    return_itc=True, decim=1, n_jobs=12, verbose=True)
+power.plot_joint(baseline=(tmin, 0), mode='mean',
+                 tmin=tmin, tmax=tmax, layout=layout,
+                 show=False)
 plt.show()
