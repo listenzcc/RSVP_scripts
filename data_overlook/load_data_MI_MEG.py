3820c0e7ffc47ff9096bb6cdd8d36f5c41b0c718
zcc <listenzcc@mail.bnu.edu.cn>
2019-04-03 09:18:35 +0800

    add data_overlook

---
+++ data_overlook/load_data_MI_MEG.py
@@ -0,0 +1,114 @@
+# code: utf-8
+
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+import mne
+import numpy as np
+import os
+import time
+
+##############
+# Parameters #
+##############
+print('Initing parameters.')
+# Results pdf path
+pdf_path = os.path.join('D:\\', 'RSVP_MEG_experiment', 'scripts',
+                        'data_overlook', 'results',
+                        'foo_%s.pdf' % time.strftime('%Y-%m-%d-%H-%M-%S'))
+if not os.path.exists(os.path.dirname(pdf_path)):
+    os.mkdir(os.path.dirname(pdf_path))
+# Parameter for read raw
+file_dir = os.path.join('D:\\', 'RSVP_MEG_experiment', 'rawdata',
+                        '20190326_MI_MEG_%s',
+                        'S%02d_lixiangTHU_20190326_%02d.ds')
+subject_name = 'maxuelin'
+subject_idx = 2
+subject_name = 'zhangchuncheng'
+subject_idx = 1
+run_idx = [1, 2]
+
+# Parameter for preprocess raw
+freq_l, freq_h = 1, 120
+fir_design = 'firwin'
+meg = True
+ref_meg = False
+exclude = 'bads'
+
+# Parameter for epochs
+event_id = dict(MI1=1, MI2=2)
+tmin, t0, tmax = -1, 0, 4
+freq = 120
+decim = 1
+reject = dict(mag=5e-12)
+stim_channel = 'UPPT001'
+
+# frequency
+n_cycles = 2
+num = 20
+
+# multi cores
+n_jobs = 12
+
+# prepare rawobject
+raw_files = [mne.io.read_raw_ctf(
+    file_dir % (subject_name, subject_idx, j), preload=True) for j in run_idx]
+raw = mne.concatenate_raws(raw_files)
+raw.filter(freq_l, freq_h, fir_design=fir_design)
+# choose channel type
+picks = mne.pick_types(raw.info, meg=meg, ref_meg=ref_meg, exclude=exclude)
+
+#############
+# Let it go #
+#############
+# Get epochs
+print('Getting epochs.')
+events = mne.find_events(raw, stim_channel=stim_channel)
+baseline = (tmin, t0)
+epochs = mne.Epochs(raw, event_id=event_id, events=events,
+                    decim=decim, tmin=tmin, tmax=tmax,
+                    picks=picks, baseline=baseline,
+                    reject=reject, preload=True)
+epochs.resample(freq, npad="auto")
+
+# Exclude abscent channels in layout
+ch_names = [e[0:5] for e in epochs.ch_names]
+layout_all = mne.find_layout(epochs.info)
+ex = [e for e in layout_all.names if e not in ch_names]
+print('Exclude channels are', ex)
+# layout_all.plot(picks=[layout_all.names.index(e) for e in ex])
+layout = mne.find_layout(epochs.info, exclude=ex)
+
+# Get evoked
+keys = [_ for _ in event_id.keys()]
+evokeds = [epochs[eid].average() for eid in keys]
+
+figures = []
+
+# Plot evoked
+print('Plotting evoked.')
+for j, e in enumerate(evokeds):
+    f = e.plot(spatial_colors=True, window_title=keys[j], show=False)
+    figures.append(f)
+    f = e.plot_joint(title=keys[j], show=False)
+    figures.append(f)
+
+# Calculate and plot temporal-frequency
+print('Caling and ploting temporal-frequency.')
+freqs = np.logspace(*np.log10([freq_l, freq_h]), num=num)
+for eid in event_id.keys():
+    power, itc = mne.time_frequency.tfr_morlet(
+        epochs[eid], freqs=freqs, n_cycles=n_cycles,
+        return_itc=True, decim=decim, n_jobs=n_jobs, verbose=True)
+    f = power.plot_joint(baseline=baseline, mode='mean',
+                         tmin=tmin, tmax=tmax, layout=layout,
+                         title=eid, show=False)
+    figures.append(f)
+
+# Saving into pdf
+print('Saving into pdf.')
+with PdfPages(pdf_path) as pp:
+    for f in figures:
+        pp.savefig(f)
+
+# Finally, we show figures.
+plt.show()
