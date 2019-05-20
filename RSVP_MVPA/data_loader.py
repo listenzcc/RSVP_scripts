9c03da01ee0f9b25e4bcfe6c5cc1a26058ba700a
zcc <listenzcc@mail.bnu.edu.cn>
2019-05-20 15:57:19 +0800

    MVPA add save epochs

--- RSVP_MVPA/data_loader.py
+++ RSVP_MVPA/data_loader.py
@@ -4,16 +4,15 @@ This script is to load MEG RSVP dataset.
 '''
 
 import mne
-import numpy as np
 import os
 import time
 
 '''
-# Function: Setting evrionment for the script
-# Output: root_path, directory of project
-# Output: time_stamp, string of beginning time of the script
-# Output: id_string, customer identifier string
-# Output: results_dir, directory for storing results
+# Function: Setting evrionment for the script.
+# Output: root_path, directory of project.
+# Output: time_stamp, string of beginning time of the script.
+# Output: id_string, customer identifier string.
+# Output: results_dir, directory for storing results.
 '''
 root_dir = os.path.join('D:\\', 'RSVP_MEG_experiment')
 time_stamp = time.strftime('%Y-%m-%d-%H-%M-%S')
@@ -23,9 +22,9 @@ if not os.path.exists(results_dir):
     os.mkdir(results_dir)
 
 '''
-# Function: Reading raw_files
-# Output: raw_files, raw files in list, every element for a run
-# Output: info, measurement information
+# Function: Reading raw_files.
+# Output: raw_files, raw files in list, every element for a run.
+# Output: info, measurement information.
 '''
 file_dir = os.path.join(root_dir, 'rawdata', '20190326_RSVP_MEG_%s',
                         'S%02d_lixiangTHU_20190326_%02d.ds')
@@ -40,8 +39,8 @@ raw_files = [mne.io.read_raw_ctf(
 info = raw_files[0].info
 
 '''
-# Function: Filter and concatenate raw_files
-# Output: raw, concatenated raw_files
+# Function: Filter and concatenate raw_files.
+# Output: raw, concatenated raw_files.
 '''
 freq_l, freq_h = 0.1, 7  # seconds
 fir_design = 'firwin'
@@ -50,14 +49,14 @@ raw = mne.concatenate_raws(
     [e.filter(freq_l, freq_h, fir_design=fir_design) for e in raw_files])
 
 '''
-# Function: Get epochs
-# Output: epochs, resampled epochs of norm and odd stimuli
+# Function: Get epochs.
+# Output: epochs, resampled epochs of norm and odd stimuli.
 '''
 picks = mne.pick_types(info, meg=True, ref_meg=False, exclude='bads')
 stim_channel = 'UPPT001'
 events = mne.find_events(raw, stim_channel=stim_channel)
 event_id = dict(odd=1, norm=2)
-tmin, t0, tmax = -0.2, 0, 1
+tmin, t0, tmax = -0.2, 0, 1  # Seconds
 freq_resample = 200  # Hz
 reject = dict(mag=5e-12)
 
@@ -65,3 +64,9 @@ epochs = mne.Epochs(raw, picks=picks, events=events, event_id=event_id,
                     tmin=tmin, tmax=tmax, baseline=(tmin, t0), detrend=1,
                     reject=reject, preload=True)
 epochs.resample(freq_resample, npad='auto')
+
+'''
+# Function: Save epochs.
+# Output: None.
+'''
+epochs.save(os.path.join(results_dir, 'epochs-epo.fif'), verbose=True)
