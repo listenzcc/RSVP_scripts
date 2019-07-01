b832ea8dd32adb6e2f14be67fe6af058316d20a6
zcc <listenzcc@mail.bnu.edu.cn>
2019-07-01 14:19:42 +0800

    simplify do_MVPA_alltime, only xdawn and pca for svm-rbf

--- RSVP_MVPA/MVPA_lr/accs_multi_classifiers/do_MVPA_alltime_meg.py
+++ RSVP_MVPA/MVPA_lr/accs_multi_classifiers/do_MVPA_alltime_meg.py
@@ -24,9 +24,6 @@ import pdb
 # Output: xdawn_pipeline, pipeline of xdawn filter.
 # Output: clf_*, classifier of svm and lr.
 '''
-
-pca = make_pipeline(mne.decoding.Vectorizer(), PCA(n_components=8))
-
 xdawn = mne.preprocessing.Xdawn(n_components=8)
 
 cv = StratifiedKFold(n_splits=10, shuffle=True)
@@ -34,6 +31,8 @@ cv = StratifiedKFold(n_splits=10, shuffle=True)
 normalize_pipeline = make_pipeline(mne.decoding.Vectorizer(), MinMaxScaler())
 
 clf_svm_rbf = svm.SVC(gamma='scale', kernel='rbf', class_weight='balanced', verbose=True)
+clf_svm_linear = svm.SVC(gamma='scale', kernel='linear', class_weight='balanced', verbose=True)
+clf_lr = LogisticRegression(class_weight='balanced', verbose=True)
 
 
 def report_results(true_label, pred_label, title=None):
@@ -63,13 +62,13 @@ read_save_stuff = {}
 
 read_save_stuff['S01'] = dict(
         range_run   = range(4, 11),
-        epochs_path = os.path.join(epochs_dir, 'eeg_S01_epochs_%d-epo.fif'),
-        report_path = os.path.join(results_dir, 'accs_eeg_S01.txt'))
+        epochs_path = os.path.join(epochs_dir, 'meg_S01_epochs_%d-epo.fif'),
+        report_path = os.path.join(results_dir, 'accs_meg_S01.txt'))
 
 read_save_stuff['S02'] = dict(
         range_run   = range(4, 12),
-        epochs_path = os.path.join(epochs_dir, 'eeg_S02_epochs_%d-epo.fif'),
-        report_path = os.path.join(results_dir, 'accs_eeg_S02.txt'))
+        epochs_path = os.path.join(epochs_dir, 'meg_S02_epochs_%d-epo.fif'),
+        report_path = os.path.join(results_dir, 'accs_meg_S02.txt'))
 
 for stuff in read_save_stuff.values():
     print('-'*80)
@@ -118,7 +117,8 @@ for stuff in read_save_stuff.values():
 
     # init preds results.
     preds_xdawn_svm_rbf = np.empty([len(labels), len(w_start)+1])
-    preds_pca_svm_rbf = np.empty([len(labels), 1])
+    preds_xdawn_svm_linear = np.empty([len(labels), len(w_start)+1])
+    preds_xdawn_lr = np.empty([len(labels), len(w_start)+1])
 
     for train, test in cv.split(epochs_data, labels):
         print('-' * 80)
@@ -127,39 +127,39 @@ for stuff in read_save_stuff.values():
         xdawn_data_train = xdawn.fit_transform(epochs[train])
         xdawn_data_test = xdawn.transform(epochs[test])
 
-        # xdawn SVM
         data_train_ = xdawn_data_train[:, :, :]
         data_test_ = xdawn_data_test[:, :, :]
 
         # SVM rbf
         clf_svm_rbf.fit(normalize_pipeline.fit_transform(data_train_), labels[train])
-        preds_xdawn_svm_rbf[test, len(w_start)] = clf_svm_rbf.predict(
-                normalize_pipeline.transform(data_test_))
+        preds_xdawn_svm_rbf[test, len(w_start)] = clf_svm_rbf.predict(normalize_pipeline.transform(data_test_))
 
-        # time resolution
+        # SVM linear
+        clf_svm_linear.fit(normalize_pipeline.fit_transform(data_train_), labels[train])
+        preds_xdawn_svm_linear[test, len(w_start)] = clf_svm_linear.predict(normalize_pipeline.transform(data_test_))
+
+        # LR
+        clf_lr.fit(normalize_pipeline.fit_transform(data_train_), labels[train])
+        preds_xdawn_lr[test, len(w_start)] = clf_lr.predict(normalize_pipeline.transform(data_test_))
+
         for j, start in enumerate(w_start):
             print(j, start)
 
+            # xdawn
             data_train_ = xdawn_data_train[:, :, start:start+w_length]
             data_test_ = xdawn_data_test[:, :, start:start+w_length]
 
             # SVM rbf
             clf_svm_rbf.fit(normalize_pipeline.fit_transform(data_train_), labels[train])
-            preds_xdawn_svm_rbf[test, j] = clf_svm_rbf.predict(
-                    normalize_pipeline.transform(data_test_))
+            preds_xdawn_svm_rbf[test, j] = clf_svm_rbf.predict(normalize_pipeline.transform(data_test_))
 
-        # PCA
-        pca_data_train = pca.fit_transform(epochs[train].get_data())
-        pca_data_test = pca.fit_transform(epochs[test].get_data())
+            # SVM linear
+            clf_svm_linear.fit(normalize_pipeline.fit_transform(data_train_), labels[train])
+            preds_xdawn_svm_linear[test, j] = clf_svm_linear.predict(normalize_pipeline.transform(data_test_))
 
-        # PCA SVM
-        data_train_ = pca_data_train
-        data_test_ = pca_data_test
-
-        # SVM rbf
-        clf_svm_rbf.fit(normalize_pipeline.fit_transform(data_train_), labels[train])
-        preds_pca_svm_rbf[test, 0] = clf_svm_rbf.predict(
-                normalize_pipeline.transform(data_test_))
+            # LR
+            clf_lr.fit(normalize_pipeline.fit_transform(data_train_), labels[train])
+            preds_xdawn_lr[test, j] = clf_lr.predict(normalize_pipeline.transform(data_test_))
 
 
     '''
@@ -169,28 +169,41 @@ for stuff in read_save_stuff.values():
 
 
     with open(fpath, 'w') as f:
-        report_svm_rbf = classification_report(
-                preds_xdawn_svm_rbf[:, len(w_start)], labels, target_names=['odd', 'norm'])
+        report_svm_rbf = classification_report(preds_xdawn_svm_rbf[:, len(w_start)], labels, target_names=['odd', 'norm'])
         print(report_svm_rbf)
-        f.writelines('\n[all_xdawn_SVM_rbf]\n')
+        f.writelines('\n[all_SVM_rbf]\n')
         f.writelines(report_svm_rbf)
 
-        report_svm_rbf = classification_report(
-                preds_pca_svm_rbf, labels, target_names=['odd', 'norm'])
-        print(report_svm_rbf)
-        f.writelines('\n[all_PCA_SVM_rbf]\n')
-        f.writelines(report_svm_rbf)
+        report_svm_linear = classification_report(preds_xdawn_svm_linear[:, len(w_start)], labels, target_names=['odd', 'norm'])
+        print(report_svm_linear)
+        f.writelines('\n[all_SVM_linear]\n')
+        f.writelines(report_svm_linear)
 
+        report_lr = classification_report(preds_xdawn_lr[:, len(w_start)], labels, target_names=['odd', 'norm'])
+        print(report_lr)
+        f.writelines('\n[all_LR]\n')
+        f.writelines(report_lr)
 
 
     for j, start in enumerate(w_start):
         print(j)
 
-        report_svm_rbf = classification_report(
-                preds_xdawn_svm_rbf[:, j], labels, target_names=['odd', 'norm'])
+        report_svm_rbf = classification_report(preds_xdawn_svm_rbf[:, j], labels, target_names=['odd', 'norm'])
         with open(fpath, 'a') as f:
             print(report_svm_rbf)
-            f.writelines('\n[%d-%d, %f, %f, xdawn_SVM_rbf]\n' % (
-                start, start+w_length, epochs.times[start], epochs.times[start+w_length]))
+            f.writelines('\n[%d-%d, %f, %f, SVM_rbf]\n' % (start, start+w_length, epochs.times[start], epochs.times[start+w_length]))
             f.writelines(report_svm_rbf)
+
+        report_svm_linear = classification_report(preds_xdawn_svm_linear[:, j], labels, target_names=['odd', 'norm'])
+        with open(fpath, 'a') as f:
+            print(report_svm_linear)
+            f.writelines('\n[%d-%d, %f, %f, SVM_linear]\n' % (start, start+w_length, epochs.times[start], epochs.times[start+w_length]))
+            f.writelines(report_svm_linear)
+
+        report_lr = classification_report(preds_xdawn_lr[:, j], labels, target_names=['odd', 'norm'])
+        with open(fpath, 'a') as f:
+            print(report_lr)
+            f.writelines('\n[%d-%d, %f, %f, LR]\n' % (start, start+w_length, epochs.times[start], epochs.times[start+w_length]))
+            f.writelines(report_lr)
+
 
